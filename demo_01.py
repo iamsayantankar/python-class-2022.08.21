33796a1ae87542b1797634452d60dc1b87eb4929
Sayantan Kar <64708708+karsayantan@users.noreply.github.com>
2022-08-29 09:07:08 +0530

    Create README.md

---
+++ demo_01.py
@@ -0,0 +1,32 @@
+import math
+
+a=127
+# b=0
+# b=int(a/12)
+
+b=15.125
+c=15.89
+
+# Print value of b and c is 15 & 16 (like round
+# print(math(b))
+print(int(c))
+x =  round(5.46543, 0)
+print(x)
+
+x =  round(5.76543, 0)
+print(x)
+
+# Print value of b and c is 15 & 15
+print(math.floor(b))
+print(math.floor(c))
+
+# Print value of b and c is 16 & 15
+
+print(math.ceil(b))
+print(math.ceil(c))
+
+
+# print(int("heuyehehe"))
+
+
+
---
+++ demo_02_swaping.py
@@ -0,0 +1,14 @@
+a=255
+b=36
+print("Value of a is: ", a)
+print("Value of b is: ", b)
+
+
+
+#Start swaping
+c=b
+b=a
+a=c
+
+print("Value of a is: ", a)
+print("Value of b is: ", b)
